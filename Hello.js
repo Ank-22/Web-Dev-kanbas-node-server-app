be1c0613cb265980908751424c1fce0c66e1c4f1
Ankit Sinha <40303711+Ank-22@users.noreply.github.com>
2024-03-28 12:29:05 -0400

    2.8 - Completed

--- Hello.js
+++ Hello.js
@@ -1,1 +1,4 @@
-console.log("Hello Ankit!")
+export default function Hello(app){
+app.get('/hello', (req, res) => {res.send('Life is good!')})
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+}
