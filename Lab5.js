fd57e38a5a283001fcec7faec56d2d0efc4e039e
Ankit Sinha <40303711+Ank-22@users.noreply.github.com>
2024-04-01 11:41:38 -0400

    3.2 Done

--- Lab5.js
+++ Lab5.js
@@ -1,3 +1,15 @@
+const assignment = {
+  id: 1, title: "NodeJS Assignment",
+  description: "Create a NodeJS server with ExpressJS",
+  due: "2021-10-10", completed: false, score: 0,
+};
+
+const module ={
+  id: "ID000",
+  name: "Ankit Sinha",
+  description: "This is my name",
+  course: "101 of ANk"
+}
 const Lab5 = (app) => {
     app.get("/a5/welcome", (req, res) => {
       res.send("Welcome to Assignment 5");
@@ -43,7 +55,29 @@ const Lab5 = (app) => {
         }
         res.send(result.toString());
       });
-    
+      app.get("/a5/assignment", (req, res) => {
+        res.json(assignment);
+      });
+      app.get("/a5/assignment/title", (req, res) => {
+        res.json(assignment.title);
+      });
+
+      app.get("/a5/assignment/title/:newTitle", (req, res) => {
+        const { newTitle } = req.params;
+        assignment.title = newTitle;
+        res.json(assignment);
+      });
+      app.get("/a5/module", (req, res) => {
+        res.json(module);
+      });
+      app.get("/a5/module/name", (req, res) => {
+        res.json(module.name);
+      });
+      app.get("/a5/module/name/:newName", (req, res) => {
+        const { newName } = req.params;
+        module.name = newName;
+        res.json(module);
+      });
   };
   export default Lab5;
   
