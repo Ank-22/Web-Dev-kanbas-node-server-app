bb41fdfe967a5af4a0c0e20570885f8cb1f51fdf
Ankit Sinha <40303711+Ank-22@users.noreply.github.com>
2024-04-02 23:51:35 -0400

    4.2.1 - Done

---
+++ Kanbas/Database/assignments.js
@@ -0,0 +1,23 @@
+export default [
+  { "_id": "A101", "title": "Propulsion Assignment", "course": "GI001", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A102", "title": "Combustion Analysis", "course": "GI001", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A103", "title": "Nozzle Design Project", "course": "GI001", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A201", "title": "Aerodynamics Quiz", "course": "GI002", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A202", "title": "Flow Analysis", "course": "GI002", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A203", "title": "Heating Analysis", "course": "GI002", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A301", "title": "Structural Design Task", "course": "GI003", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A302", "title": "Orbital Calculations", "course": "GI003", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A303", "title": "Systems Engineering Exam", "course": "GI003", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A401", "title": "Aerodynamics Quiz", "course": "GI004", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A402", "title": "Flow Analysis", "course": "GI004", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A403", "title": "Heating Analysis", "course": "GI004", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A501", "title": "Aerodynamics Quiz", "course": "GI005", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A502", "title": "Flow Analysis", "course": "GI005", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A503", "title": "Heating Analysis", "course": "GI005", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A601", "title": "Aerodynamics Quiz", "course": "GI006", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A602", "title": "Flow Analysis", "course": "GI006", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A603", "title": "Heating Analysis", "course": "GI006", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A701", "title": "Aerodynamics Quiz", "course": "GI007", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A702", "title": "Flow Analysis", "course": "GI007", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 },
+  { "_id": "A703", "title": "Heating Analysis", "course": "GI007", "subtitle": "Something interesting", "dueDate": "12/12/24", "points": 100 }
+]
---
+++ Kanbas/Database/enrollments.js
@@ -0,0 +1,58 @@
+export default [ { "_id": "1", "user": "121", "course": "GI001" },
+  { "_id": "2", "user": "122", "course": "GI001" },
+  { "_id": "3", "user": "123", "course": "GI001" },
+  { "_id": "4", "user": "124", "course": "GI001" },
+  { "_id": "5", "user": "125", "course": "GI001" },
+  { "_id": "6", "user": "126", "course": "GI001" },
+  { "_id": "7", "user": "127", "course": "GI001" },
+
+    { "_id": "8", "user": "121", "course": "GI002" },
+    { "_id": "9", "user": "122", "course": "GI002" },
+    { "_id": "10", "user": "123", "course": "GI002" },
+    { "_id": "11", "user": "124", "course": "GI002" },
+    { "_id": "12", "user": "125", "course": "GI002" },
+    { "_id": "13", "user": "126", "course": "GI002" },
+    { "_id": "14", "user": "127", "course": "GI002" },
+
+    { "_id": "15", "user": "121", "course": "GI003" },
+    { "_id": "16", "user": "122", "course": "GI003" },
+    { "_id": "17", "user": "123", "course": "GI003" },
+    { "_id": "18", "user": "124", "course": "GI003" },
+    { "_id": "19", "user": "125", "course": "GI003" },
+    { "_id": "20", "user": "126", "course": "GI003" },
+    { "_id": "21", "user": "127", "course": "GI003" }, 
+
+    { "_id": "22", "user": "121", "course": "GI004" },
+    { "_id": "23", "user": "122", "course": "GI004" },
+    { "_id": "24", "user": "123", "course": "GI004" },
+    { "_id": "25", "user": "124", "course": "GI004" },
+    { "_id": "26", "user": "125", "course": "GI004" },
+    { "_id": "27", "user": "126", "course": "GI004" },
+    { "_id": "28", "user": "127", "course": "GI004" }, 
+
+    { "_id": "28", "user": "121", "course": "GI005" },
+    { "_id": "29", "user": "122", "course": "GI005" },
+    { "_id": "30", "user": "123", "course": "GI005" },
+    { "_id": "31", "user": "124", "course": "GI005" },
+    { "_id": "32", "user": "125", "course": "GI005" },
+    { "_id": "33", "user": "126", "course": "GI005" },
+    { "_id": "34", "user": "127", "course": "GI005" },
+
+    { "_id": "34", "user": "121", "course": "GI006" },
+    { "_id": "35", "user": "122", "course": "GI006" },
+    { "_id": "36", "user": "123", "course": "GI006" },
+    { "_id": "37", "user": "124", "course": "GI006" },
+    { "_id": "38", "user": "125", "course": "GI006" },
+    { "_id": "39", "user": "126", "course": "GI006" },
+    { "_id": "40", "user": "127", "course": "GI006" },
+
+    { "_id": "41", "user": "121", "course": "GI007" },
+    { "_id": "42", "user": "122", "course": "GI007" },
+    { "_id": "43", "user": "123", "course": "GI007" },
+    { "_id": "44", "user": "124", "course": "GI007" },
+    { "_id": "45", "user": "125", "course": "GI007" },
+    { "_id": "46", "user": "126", "course": "GI007" },
+    { "_id": "47", "user": "127", "course": "GI007" }
+
+]
+
