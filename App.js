1bec93add7b6b4f583e1c9d30de91fa3359ca96f
Ankit Sinha <40303711+Ank-22@users.noreply.github.com>
2024-04-18 00:31:17 -0400

    User Working

--- App.js
+++ App.js
@@ -7,12 +7,34 @@ import cors from "cors";
 import AssignmentRoutes from './Kanbas/Assignment/routes.js';
 import mongoose from "mongoose";
 import UserRoutes from './Users/routes.js';
+import session from "express-session";
+import "dotenv/config";
+
 
 
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: process.env.FRONTEND_URL,
+}));
 app.use(express.json());
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.HTTP_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+
+  
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
